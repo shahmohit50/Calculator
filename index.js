5e8f4a5da46a43d71b29e32f19dd682d8d05a168
Mohitraj Shah <shahmohit50@gmail.com>
2021-10-31 20:54:14 +0530

    Message

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function minus() {
+    
+}
+function add() {
+    
+}
+function divide() {
+    
+}
+function mul() {
+    
+
+}
+function equal() {
+    
+}
+function update(parms) {
+    console.log(parms);
+    var task = document.getElementById("task");
+    task.innerHtml += parms; 
+}
+function clear() {
+    var task = document.getElementById("task");
+    task.innerHtml =""; 
+}

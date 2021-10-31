b31517484147647adc28680d282ce600d002b6c8
Mohitraj Shah <shahmohit50@gmail.com>
2021-10-31 21:38:56 +0530

    Message

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 
 function minus() {
-    
+    var task = document.getElementById("task");
+    task.innerHTML += "-"; 
 }
 function add() {
-    
+    var task = document.getElementById("task");
+    task.innerHTML += "+"; 
 }
 function divide() {
-    
+    var task = document.getElementById("task");
+    task.innerHTML += "/"; 
 }
 function mul() {
-    
+    var task = document.getElementById("task");
+    task.innerHTML += "*"; 
 
 }
 function equal() {
-    
+    var task = document.getElementById("task");
+    var sum = task.innerHTML;
+    console.log(sum);
+    task.innerHTML =""; 
 }
 function update(parms) {
     console.log(parms);
     var task = document.getElementById("task");
-    task.innerHtml += parms; 
+    task.innerHTML += parms; 
 }
 function clear() {
     var task = document.getElementById("task");
-    task.innerHtml =""; 
+    task.innerHTML =""; 
 }
